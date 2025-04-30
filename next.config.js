bdc7dac478736a3229524687b4cf0a96753bddd2
Lancelot Grafilo <olifarg12345@gmail.com>
2025-04-30 14:37:07 +0800

    updated and working market

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "utfs.io",
       },
+      {
+        protocol: "https",
+        hostname: "s3.amazonaws.com"
+      }
     ],
   },
 };
